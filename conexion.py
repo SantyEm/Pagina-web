036734443fd2dbd22c7976ff6914b003820ea5a8
SantyEm <135279596+SantyEm@users.noreply.github.com>
2024-11-14 22:34:14 -0300

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 
 def get_connection():
-            config = {
+    config = {
             'host': 'junction.proxy.rlwy.net',
             'user': 'root',
             'password': 'qCEFPRFRQtRjGdVhZDGJTyaDnrBeBKAN',
